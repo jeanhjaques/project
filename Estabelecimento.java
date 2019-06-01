667a49e16a5abc2bd2f7cedd633ce6d3f3f8223a
jeanhjaques <45767643+jeanhjaques@users.noreply.github.com>
2019-06-01 17:44:09 -0400

    Update Estabelecimento.java
    Adição de atributos e construtor a classe Estabelecimento.

--- Estabelecimento.java
+++ Estabelecimento.java
@@ -1,2 +1,20 @@
 public class Estabelecimento{
+  //atributos
+  private String nomeEstabelecimento;
+  private String descricaoEstabelecimento;
+  private String horarioAbertura;
+  private String horarioEncerramento;
+  private Endereco endereco;
+  private Cardapio cardapio;
+  
+  //construtor
+  public Estabelecimento(String nomeEstabelecimento,String descricaoEstabelecimento, String horarioAbertura,
+                         String horarioEncerramento, Endereco endereco, Cardapio cardapio){
+    this.nomeEstabelecimento = nomeEstabelecimento;
+    this.descricaoEstabelecimento = descricaoEstabelecimento;
+    this.horarioAbertura = horarioAbertura;
+    this.horarioEncerramento = horarioEncerramento;
+    this.endereco = endereco;
+    this.cardapio = cardapio;
+  }
 }
