4d9278b1de260d9d54e3f63af61c355a116f83bd
jeanhjaques <45767643+jeanhjaques@users.noreply.github.com>
2019-06-01 18:46:09 -0400

    Update Estabelecimento.java
    Adição dos getters, setters e toString.

--- Estabelecimento.java
+++ Estabelecimento.java
@@ -17,4 +17,60 @@ public class Estabelecimento{
     this.endereco = endereco;
     this.cardapio = cardapio;
   }
+
+  //getters e setters
+  public String getNomeEstabelecimento(){
+  	return nomeEstabelecimento;
+  }
+
+  public void setNomeEstabelecimento(String nomeEstabelecimento){
+  	this.nomeEstabelecimento = nomeEstabelecimento;
+  }
+
+  public String getDescricaoEstabelecimento(){
+  	return descricaoEstabelecimento;
+  }
+
+  public void setDescricaoEstabelecimento(String descricaoEstabelecimento){
+  	this.descricaoEstabelecimento = descricaoEstabelecimento;
+  }
+
+  public String getHorarioAbertura(){
+  	return horarioAbertura;
+  }
+
+  public void setHorarioAbertura(String horarioAbertura){
+  	this.horarioAbertura = horarioAbertura;
+  }
+
+  public String getHorarioEncerramento(){
+  	return horarioEncerramento;
+  }
+
+  public void setHorarioEncerramento(String horarioEncerramento){
+  	this.horarioEncerramento = horarioEncerramento;
+  }
+
+  public Endereco getEndereco(){
+  	return endereco;
+  }
+
+  public void setEndereco(Endereco endereco){
+  	this.endereco = endereco;
+  }
+
+  public Cardapio getCardapio(){
+  	return cardapio;
+  }
+
+  public void setCardapio(Cardapio cardapio){
+  	this.cardapio = cardapio;
+  }
+
+  //toString
+  @Override
+  public String toString(){
+  	return ("nomeEstabelecimento: "+nomeEstabelecimento+"/n descricaoEstabelecimento: "+descricaoEstabelecimento+"/n horarioAbertura: "+
+  			horarioAbertura+"/n horarioEncerramento: "+horarioEncerramento+"/n endereco: "+endereco+"cardapio: "+cardapio);
+  }
 }
