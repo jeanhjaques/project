683179f94204350c83440f29c35daab3a63acd2a
jeanhjaques <45767643+jeanhjaques@users.noreply.github.com>
2019-06-02 01:24:09 -0400

    Update Estabelecimento.java
    Correção de falhas do commit anterior

--- Estabelecimento.java
+++ Estabelecimento.java
@@ -4,28 +4,18 @@ public class Estabelecimento{
   private String descricaoEstabelecimento;
   private String horarioAbertura;
   private String horarioEncerramento;
-<<<<<<< HEAD
   private int codigoEstabelecimento;
-=======
->>>>>>> 4d9278b1de260d9d54e3f63af61c355a116f83bd
   private Endereco endereco;
   private Cardapio cardapio;
   
   //construtor
   public Estabelecimento(String nomeEstabelecimento,String descricaoEstabelecimento, String horarioAbertura,
-<<<<<<< HEAD
                          String horarioEncerramento, Endereco endereco, Cardapio cardapio, int codigoEstabelecimento){
-=======
-                         String horarioEncerramento, Endereco endereco, Cardapio cardapio){
->>>>>>> 4d9278b1de260d9d54e3f63af61c355a116f83bd
     this.nomeEstabelecimento = nomeEstabelecimento;
     this.descricaoEstabelecimento = descricaoEstabelecimento;
     this.horarioAbertura = horarioAbertura;
     this.horarioEncerramento = horarioEncerramento;
-<<<<<<< HEAD
     this.codigoEstabelecimento = codigoEstabelecimento;
-=======
->>>>>>> 4d9278b1de260d9d54e3f63af61c355a116f83bd
     this.endereco = endereco;
     this.cardapio = cardapio;
   }
@@ -62,14 +52,11 @@ public class Estabelecimento{
   public void setHorarioEncerramento(String horarioEncerramento){
   	this.horarioEncerramento = horarioEncerramento;
   }
-
-<<<<<<< HEAD
+  
   public int getCodigoEstabelecimento(){
   	return codigoEstabelecimento;
   }
   
-=======
->>>>>>> 4d9278b1de260d9d54e3f63af61c355a116f83bd
   public Endereco getEndereco(){
   	return endereco;
   }
@@ -89,14 +76,7 @@ public class Estabelecimento{
   //toString
   @Override
   public String toString(){
-<<<<<<< HEAD
   	return "Nome: "+nomeEstabelecimento+"\nDescrição: "+descricaoEstabelecimento+"\nAbre: "+
   			horarioAbertura+"\nFecha: "+horarioEncerramento+"\nEndereço: "+endereco+"\nCardápio: "+cardapio+"\n";
- }
-}
-=======
-  	return ("nomeEstabelecimento: "+nomeEstabelecimento+"/n descricaoEstabelecimento: "+descricaoEstabelecimento+"/n horarioAbertura: "+
-  			horarioAbertura+"/n horarioEncerramento: "+horarioEncerramento+"/n endereco: "+endereco+"cardapio: "+cardapio);
   }
 }
->>>>>>> 4d9278b1de260d9d54e3f63af61c355a116f83bd
